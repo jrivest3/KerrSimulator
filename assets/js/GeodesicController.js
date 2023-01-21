af7af31013e6176d049b05b4ac7feae153035aa2
Joe Rivest <ljrivest@go.olemiss.edu>
2023-01-21 14:25:47 -0600

    Easier to make inc=0

--- assets/js/GeodesicController.js
+++ assets/js/GeodesicController.js
@@ -31,8 +31,8 @@ class GeodCont {
                     
                 }
             });
-        this.spin = this.ctrlbrd.create('slider', [[.8, 1.15], [17, 1.15], [0, 0, .99]], { name: 'spin' });
-        this.inc = this.ctrlbrd.create('slider', [[.8, 1.05], [17, 1.05], [-1, -1, 1]], { name: 'inclination', snapWidth: .01 });
+        this.spin = this.ctrlbrd.create('slider', [[.8, 1.15], [17, 1.15], [0, 0.9, .99]], { name: 'spin' });
+        this.inc = this.ctrlbrd.create('slider', [[.8, 1.05], [17, 1.05], [-1, 0, 1]], { name: 'inclination', snapWidth: .1 });
 
         this.SepCurve = new SepController(this.ctrlbrd);
         const SepCurve = that.SepCurve;
